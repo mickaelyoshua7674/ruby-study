c4e4b2c75bd83e95ef6580b37dd7e6cd228ace3f
Mickael Yoshua <mickaelyoshua@gmail.com>
2025-03-31 19:12:26 +0000

    if else

--- helloWorld.rb
+++ helloWorld.rb
@@ -1,2 +1,10 @@
-name = "Yoshua"
-puts "Mickael #{name}"
+print "What is age? "
+age = gets.to_i
+
+if age <= 18
+  puts "Can not make a tatoo"
+elsif age>18 && age<=20
+  puts "Maybe can make a tatoo"
+else
+  puts "Definitely can make tatoo"
+end
